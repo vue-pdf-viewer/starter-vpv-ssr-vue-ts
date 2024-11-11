26be619e3ed4dbb5449b1ff9944b02a5aa84ea4b
haroonth <kittisak.ma@logicspark.com>
2024-11-11 13:57:40 +0700

    📝 add how to use `useLicense`

--- src/components/PdfViewer.vue
+++ src/components/PdfViewer.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
-import { VPdfViewer } from '@vue-pdf-viewer/viewer';
+	import { onBeforeMount, watch, ref } from "vue";
+	import { VPdfViewer, useLicense } from "@vue-pdf-viewer/viewer";
+
+	const vpvRef = ref<InstanceType<typeof VPdfViewer> | null>(null);
+	onBeforeMount(() => {
+		useLicense({ licenseKey: "your-license-key" });
+	});
+	watch(
+		vpvRef,
+		(newVal) => {
+			console.log("These are VPV instance properties", Object.keys(newVal));
+		},
+		{ deep: true }
+	);
 </script>
 <template>
-  <VPdfViewer
-    v-bind="$attrs"
-    src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+	<VPdfViewer
+		v-bind="$attrs"
+		ref="vpvRef"
+		src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
 </template>
